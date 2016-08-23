b998eb1dc808143221673472862c4ea034117393
SaurabhSakpal <sfstoe@gmail.com>
2016-08-23 09:20:25 -0400

    Adding the unit test module

---
+++ code/1/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import utest
+from saurabh import multiply
+
+def add(firstNumber, secondNumber) :
+	return firstNumber + secondNumber
+
+print "Value of addition is ", add(4, 5)
+
+print "Value of multiplication is ", multiply(4, 5)
---
+++ code/1/saurabh.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+def multiply(firstNumber, secondNumber):
+	return firstNumber * secondNumber
