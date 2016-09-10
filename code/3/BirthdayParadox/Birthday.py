4b3b196ec8e0da14c2d2f92b2d230f1a45b28e40
SaurabhSakpal <sfstoe@gmail.com>
2016-09-09 23:53:04 -0400

    abc

--- code/3/BirthdayParadox/Birthday.py
+++ code/3/BirthdayParadox/Birthday.py
@@ -4,9 +4,9 @@ def has_duplicates(list):
 	new_dict = {}
 	for i in range(len(list)):
 		if new_dict.has_key(list[i]):
-			return False
+			return True
 		new_dict[list[i]] = 1
-	return True;
+	return False;
 
 
 def birthdayParadox(numberOfPeople, numberOfSamples) :
@@ -30,10 +30,22 @@ def birthdayParadox(numberOfPeople, numberOfSamples) :
 listWithOutDuplicate = [2, 3, 4, 5, 6]
 listWithDuplicate = [2, 3, 4, 5, 6, 4]
 
-print 'Does folowing list : % has duplicates % ',listWithOutDuplicate, has_duplicates(listWithDuplicate)
-print has_duplicates(listWithOutDuplicate)
+str1 = ' '.join(str(e) for e in listWithOutDuplicate)
+str2 = ' '.join(str(e) for e in listWithDuplicate)
+
+str3 = "Does folowing list " + str1
+str3 = str3 + " has duplicates: " + str(has_duplicates(listWithOutDuplicate))
+print str3
+
+
+str4 = "Does folowing list " + str2
+str4 = str4 + " has duplicates: " + str(has_duplicates(listWithDuplicate))
+print str4
 
 numberOfPeople = 23
 numberOfSamples = 100
 
-print birthdayParadox(numberOfPeople, numberOfSamples)
+str5 =  "Probabilty of two people having same bday is "+str(birthdayParadox(numberOfPeople, numberOfSamples))
+str5 = str5 + " number of people = "+str(numberOfPeople)+" and number Of Runs = "+str(numberOfSamples)
+
+print str5
