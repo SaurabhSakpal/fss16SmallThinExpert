5024986cc69fe2f7863435a9b873f3b9d3d6d587
SaurabhSakpal <sfstoe@gmail.com>
2016-09-09 23:23:57 -0400

    Merging the master Code

--- code/3/poker/PokerHand.py
+++ code/3/poker/PokerHand.py
@@ -34,6 +34,14 @@ class PokerHand(Hand):
 				return True
 		return False
 
+	def checkIfStraight(self, sorted_cards):
+		if sorted_cards[0]==1:
+			sorted_cards.append(14)
+		for i in range(4,len(sorted_cards)):
+			if sorted_cards[i]-sorted_cards[i-4] == 4:
+				return True;
+		return False;
+
 	def has_straight_flush(self):
 		"""Returns True if the hand has a straight flush, False otherwise.
 		"""
@@ -41,12 +49,32 @@ class PokerHand(Hand):
 			if len(card_list) < 5:
 				continue
 			sorted_cards = sorted(card_list)
-			if sorted_cards[0]==1:
-				sorted_cards.append(14)
-			for i in range(4,len(sorted_cards)):
-				if sorted_cards[i]-sorted_cards[i-4] == 4:
-					return True;
+			if self.checkIfStraight(sorted_cards):
+				return True;
 		return False
+
+
+	def straight(self) :
+		""" Returns True if the hand has a straight, False otherwise 
+		"""
+		cumulativeList = [];
+		for card_list in self.suits.values():
+			if card_list != None :
+				for j in range(len(card_list)) :
+					cumulativeList.append(card_list[j])
+		print cumulativeList
+		sorted_cards = sorted(cumulativeList)
+		previous = -1
+		unduplicated_list = []
+		for i in range(len(sorted_cards)):
+			if previous != sorted_cards[i] :
+				unduplicated_list.append(sorted_cards[i])
+				previous = sorted_cards[i]
+		print unduplicated_list
+		if self.checkIfStraight(unduplicated_list):
+			return True;
+		return False
+
 
 
 if __name__ == '__main__':
@@ -62,10 +90,12 @@ if __name__ == '__main__':
 		# hand.suit_hist()
 		hand2 = PokerHand()
 		hand2.add_card(Card(0,1))
-		hand2.add_card(Card(0,10))
-		hand2.add_card(Card(0,11))
-		hand2.add_card(Card(0,12))
-		hand2.add_card(Card(0,13))
+		hand2.add_card(Card(1,1))
+		hand2.add_card(Card(1,10))
+		hand2.add_card(Card(1,7))
+		hand2.add_card(Card(1,12))
+		hand2.add_card(Card(1,13))
+		hand2.add_card(Card(1,11))
 		hand2.suit_hist()
 		print hand2
 		print hand2.has_straight_flush()
