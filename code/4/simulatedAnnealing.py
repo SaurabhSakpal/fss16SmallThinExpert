edebf3935d272b237ddd806470994d53720d99ac
SaurabhSakpal <sfstoe@gmail.com>
2016-10-15 15:55:55 -0400

    adding comments to simulated annealing

--- code/4/simulatedAnnealing.py
+++ code/4/simulatedAnnealing.py
@@ -6,7 +6,7 @@ def say(x):
     print(x, end="")
 
 def energy(x):
-    
+    """ we need to calculate the energy of the point at x using schaffer function """
     f1 = pow(x, 2)
     f2 = pow(x-2, 2)
     #print(f1)
@@ -16,11 +16,11 @@ def energy(x):
     return energy
 
 def probability(e, en, ratio):
-    
+    """ This method calculates the probability by which we do random jump """
     return pow( math.e, (e-en) / ratio )
     
 def neighbor(x):
-    
+    """ This method returns a random neighbor of the current point """
     while True:
         step = 5
         xn = x + random.randint(-1*step, step)
@@ -28,7 +28,7 @@ def neighbor(x):
             return xn
 
 def randneighbor():
-    
+    """ When we have to random jump to a different solution we have to choose a random point from entire range """
     while True:
         xn = random.randint(int(pow(10, -5)), int(pow(10, 5)))
         if xn > pow(10, -5) and xn < pow(10, 5):
@@ -59,17 +59,19 @@ def simulatedannealing(x0):
         en = energy(sn)
 
         if en < eb:
+            # new best solution has been found
             eb = en
             sb = sn
             say('!')
         
         if en < e:
+            # energy of neighbour better than current so update the current
             s = sn
             e = en
             say('+')
             
         elif probability(e, en, (float(k)/float(kmax))) < random.random():
-            
+            # make a random jump to escape the local maxima            
             s = randneighbor()
             e = energy(s)
             say('?')
